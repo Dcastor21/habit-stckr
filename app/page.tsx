f096d6f637f23207c842fa86437f1322cb25966d
Darnel Castor <darnelcastor2@gmail.com>
2024-07-25 00:00:51 -0400

    add Poppins font

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+      <div>
+        <h1 className="poppins font-bold">test</h1>
+      </div>
     </main>
   );
 }
