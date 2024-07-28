4503b9e5747306327792322edcd8cdf75e32f88d
Darnel Castor <darnelcastor2@gmail.com>
2024-07-27 20:54:23 -0400

    building the Stdebar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,43 @@
 "use client";
-
-import React from "react";
+import { useGlobalContextProvider } from "../contextApi";
+import { menuItemType } from "../Types/MenuItemType";
+import Areas from "../Pages/Areas/Areas";
+import AllHabits from "../Pages/AllHabits/AllHabits";
+import Statistics from "../Pages/Statistics/Statistics";
+import React, { useEffect, useState } from "react";
 import { SignOutButton, useUser } from "@clerk/nextjs";
 import Sidebar from "../Components/SideBar/Sidebar";
 
 function Dashboard() {
+  const { menuItemsObject } = useGlobalContextProvider();
+  const { menuItems } = menuItemsObject;
+  const [selectMenu, setSelectedMenu] = useState<menuItemType | null>(null);
+  let selectCompenent = null;
+
+  useEffect(() => {
+    menuItems.map((singleItem) => {
+      if (singleItem.isSelected) {
+        setSelectedMenu(singleItem);
+      }
+    });
+  }, [menuItems]);
+
+  switch (selectMenu?.name) {
+    case "All Areas":
+      selectCompenent = <Areas />;
+      break;
+    case "All Habits":
+      selectCompenent = <AllHabits />;
+      break;
+    case "Statistics":
+      selectCompenent = <Statistics />;
+      break;
+  }
   const { user } = useUser();
   return (
     <div className="flex">
       <Sidebar />
-      <div>hello,{user?.lastName}</div>
+      {selectCompenent}
     </div>
   );
 }
