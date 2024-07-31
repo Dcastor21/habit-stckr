832f52d2354860bf97e32908a40a68287658064c
Darnel Castor <darnelcastor2@gmail.com>
2024-07-31 13:41:33 -0400

    fixing the sidebar

--- app/Pages/AllHabits/AllHabits.tsx
+++ app/Pages/AllHabits/AllHabits.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { faMoon, faSearch, faSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useUser } from "@clerk/nextjs";
 import AllHabitsSearchBar from "./AllHabitsSearchBar";
 import AllHabitsTopBar from "./AllHabitsTopBar";
 import AllHabitsRightSideBar from "./AllHabitsRightSideBar";
 import HabitsContainer from "./HabitsContainer";
+import HabitsCompleted from "./HabitsCompleted";
 
 function AllHabits() {
-  const { user } = useUser();
   return (
     <div className="w-full flex">
       <div className="w-[80%] m-5">
         <AllHabitsTopBar />
         <HabitsContainer />
+        <HabitsCompleted />
       </div>
       <AllHabitsRightSideBar />
     </div>
--- app/Pages/AllHabits/HabitsCompleted.tsx
+++ app/Pages/AllHabits/HabitsCompleted.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Checkbox, IconButton } from "@mui/material";
+import { defaultColor } from "@/colors";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 
 function HabitsCompleted() {
-  return <div>HabitsCompleted</div>;
+  return (
+    <div className="bg-white mt-7 p-8 rounded-md">
+      <span className="font-bold text-lg mb-2">Habits Completed</span>
+      <div className="mt-4 opacity-50">
+        <HabitCard />
+        <HabitCard />
+      </div>
+    </div>
+  );
 }
 
 export default HabitsCompleted;
+
+function HabitCard() {
+  return (
+    <div className="flex p-3 items-center justify-between">
+      <Checkbox
+        icon={<RadioButtonUncheckedIcon />}
+        checkedIcon={<CheckCircleIcon />}
+        sx={{
+          color: defaultColor.default,
+          "&.Mui-checked": { color: defaultColor.default },
+        }}
+      />
+
+      <div className="flex justify-between gap-2 w-full p-3 py-4 rounded-md bg-slate-50 border">
+        <div className="w-full">
+          <div className="flex gap-2 justify-between border">
+            <div className="flex gap-2 items-center">
+              <FontAwesomeIcon
+                className="p-3 rounded-full w-4 h-4 bg-customRed text-white"
+                height={20}
+                width={20}
+                icon={faCode}
+              />
+              <span className="">Coding</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
