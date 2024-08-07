c12d67b4f0b3c91c29bb7e460e1201bc3558810c
Darnel Castor <darnelcastor2@gmail.com>
2024-08-07 14:01:27 -0400

    sidebar

--- app/Pages/AllHabits/AllHabitsRightSideBar.tsx
+++ app/Pages/AllHabits/AllHabitsRightSideBar.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { PieChart, Pie, Cell } from "recharts";
+import { defaultColor } from "@/colors";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import UserProfile from "./RightSidebar/UserProfile";
+import MainStatistics from "./RightSidebar/MainStatistics";
 
 function AllHabitsRightSideBar() {
-  return <div className="w-[20%] bg-white"> </div>;
+  return (
+    <div className="w-[20%] bg-white">
+      <UserProfile />
+      <MainStatistics />
+    </div>
+  );
 }
 
 export default AllHabitsRightSideBar;
